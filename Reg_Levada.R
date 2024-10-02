35c87ef0fb352393d54f29932e2c3dd5d5ee00b0
Oleksandr Valchyshen <34207884+valchyshen@users.noreply.github.com>
2024-10-02 05:48:08 -0400

    Update Reg_Levada.R

--- Reg_Levada.R
+++ Reg_Levada.R
@@ -1,6 +1,6 @@
 #
 #      Oleksandr Valchyshen
-#      avpfc@mail.umkc.edu, ovalchyshen@bard.edu
+#      avpfc@mail.umkc.edu
 #
 #      Started: Sep 25, 2024
 #
@@ -18,6 +18,7 @@ name_pdf1  <- "war_russians1.pdf"
 name_pdf2  <- "war_russians2.pdf"
 name_pdf4  <- "war_russians4.pdf"
 name_pdf5  <- "war_russians5.pdf"
+name_pdf6  <- "war_russians6.pdf"
 
 hd       <- "C:/UMKC/My Working Papers/Dissertation/Data/"
 lvd.csv  <- "Levada.csv"  # https://www.levada.ru/en/ratings/approval-of-the-authorities/
@@ -92,8 +93,26 @@ boxplot(fd.ts.ptn,  main="Putin Approval (PA), 1st Difference", cex.axis=.8);
 par(); dev.off()
 
 # Plotting two net indexes for stationarity: Gen Sit/Cond & Putin App 
+pdf(file=paste0(path_plots,name_pdf6))
+par(mfrow=c(2,4), omi=c(0, 0, 0, 0), mar=c(4, 4, 4, 2))
+# Gen Sit/Cond 
+plot.ts(lvd.ts.ges[,1], 
+        main="Gen.Conditions (GC)",
+        xlab=paste("Time: # of obs =",length(lvd.ts.ges[,1])), ylab="Approve")
+acf(lvd.ts.ges[,1], main="ACF: GC")
+plot.ts(fd.ts.ges[,1], main="1st Diff. of GC", xlab=paste("Time: # of obs =",length(fd.ts.ges[,1])), ylab="")
+acf(fd.ts.ges[,1], main="ACF: 1st Diff. of GC")
+# Putin App 
+plot.ts(lvd.ts.ptn[,1], 
+        main="Putin Approval (PA)",
+        xlab=paste("Time: # of obs =",length(lvd.ts.ptn[,1])), ylab="Approve")
+acf(lvd.ts.ptn[,1], main="ACF: PA")
+plot.ts(fd.ts.ptn[,1], main="1st Diff. of PA", xlab=paste("Time: # of obs =",length(fd.ts.ptn[,1])), ylab="")
+acf(fd.ts.ptn[,1], main="ACF: 1st Diff. of PA")
+par(); dev.off()
+
 pdf(file=paste0(path_plots,name_pdf1))
-par(mfrow=c(2,4), mar=c(2, 2, 2, 2), omi=c(0.2, 0, 0, 0))
+par(mfrow=c(2,4), omi=c(0, 0, 0, 0), mar=c(4, 4, 4, 2))
 # Gen Sit/Cond 
 plot.ts(lvd.ts.ges[,4], 
         main="Gen.Conditions (GC)",
@@ -112,15 +131,25 @@ par(); dev.off()
 
 # Plotting of Levada Surveys' Key Approval Indexes
 pdf(file=paste0(path_plots,name_pdf2))
-par(mfrow=c(1,3), mar=c(2, 2, 2, 2), omi=c(0.2, 0, 0, 0))
-plot.ts(fd.ts.ges[,4], 
+par(mfrow=c(2,3), mar=c(2, 2, 2, 2), omi=c(0, 0, 0, 0))
+plot.ts(fd.ts.ges[,1], 
         main="General Conditions (GC)",
+        xlab=paste("Time: Number of obs =",length(fd.ts.ges[,1])),
+        ylab="First difference: Net")
+plot.ts(fd.ts.ptn[,1], main="Putin Approval (PA)",
+        xlab=paste("Time: Number of obs =",length(fd.ts.ptn[,1])),
+        ylab="First difference: Net")
+plot.ts(ts.war_dummy1, main="War Dummy",
+        xlab=paste("Time: Number of obs =",length(ts.war_dummy1)),
+        ylab="0 = Peace & Old Wars / 1 = New War")
+plot.ts(fd.ts.ges[,4], 
+        main="General Conditions (GC), Net",
         xlab=paste("Time: Number of obs =",length(fd.ts.ges[,4])),
         ylab="First difference: Net")
-plot.ts(fd.ts.ptn[,4], main="Putin Approval (PA)",
+plot.ts(fd.ts.ptn[,4], main="Putin Approval (PA), Net",
         xlab=paste("Time: Number of obs =",length(fd.ts.ptn[,4])),
         ylab="First difference: Net")
-plot.ts(ts.war_dummy1, main="War Dummy #1",
+plot.ts(ts.war_dummy1, main="War Dummy",
         xlab=paste("Time: Number of obs =",length(ts.war_dummy1)),
         ylab="0 = Peace & Old Wars / 1 = New War")
 par(); dev.off()
@@ -131,36 +160,67 @@ summary(fd.ts.ptn)
 # Align all time series to the Putin data
 d <- as.Date(time(fd.ts.ptn))[1]
 ges <- window(fd.ts.ges, start=c(year(d),month(d)))[,4]
-war_dummy1 <- window(ts.war_dummy1, start=c(year(d),month(d)))
+war_dummy <- window(ts.war_dummy1, start=c(year(d),month(d)))
 war_dummy2 <- window(ts.war_dummy2, start=c(year(d),month(d)))
 war_area <- window(ts.war_area, start=c(year(d),month(d)))
-ptn <- fd.ts.ptn[,4]
-length(ges); length(ptn); length(war_dummy1); length(war_dummy2); length(war_area)
+pa     <- fd.ts.ptn[,1];  pa.adf <- adf.test(pa)
+pa.net <- fd.ts.ptn[,4];  pa.net.adf <- adf.test(pa.net)
+length(ges); length(pa); length(war_dummy); length(war_dummy2); length(war_area)
 
 #m1 <- lm(ges ~ ptn)
 #summary(m1)
 
-m2 <- lm(ptn ~ war_dummy1)
-summary(m2)
+m2 <- lm(pa ~ war_dummy)
+#summary(m2)
 
-m3 <- lm(ptn ~ war_dummy1 + war_area)
-summary(m3)
+m3 <- lm(pa.net ~ war_dummy)
+#summary(m3)
 
 # Align all time series to the Gen Situation/Conditions data
 d <- as.Date(time(fd.ts.ges))[1]
-ges <- fd.ts.ges[,4]
-war_dummy1 <- window(ts.war_dummy1, start=c(year(d),month(d)))
+gc     <- fd.ts.ges[,1];  gc.adf <- adf.test(gc);
+gc.net <- fd.ts.ges[,4];  gc.nt.adf <- adf.test(gc.net)
+
+war_dummy <- window(ts.war_dummy1, start=c(year(d),month(d)))
 war_dummy2 <- window(ts.war_dummy2, start=c(year(d),month(d)))
 war_area <- window(ts.war_area, start=c(year(d),month(d)))
-length(ges); length(war_dummy1); length(war_dummy2); length(war_area)
+length(gc); length(war_dummy); length(war_dummy2); length(war_area)
 
-m4 <- lm(ges ~ war_dummy1)
-summary(m4)
+m4 <- lm(gc ~ war_dummy)
+#summary(m4)
 
-m5 <- lm(ges ~ war_dummy1 + war_area)
-summary(m5)
+m5 <- lm(gc.net ~ war_dummy)
+#summary(m5)
+
+# --- Dr. Li suggestions -------------------------------------------------------+
+# You only showed the stationarity check via the visual detector; can you do a 
+# typical Advanced Dickey-Fuller test or other type of unit root test and report 
+# the result? Autocorrelation needs a formal test.
+#
+# Using only one dummy variable as the only explanatory variable in a time series 
+# model can lead to over-fitting in the periods it applies to and under-fitting 
+# elsewhere. This limits the model's ability to generalize or explain variations 
+# beyond the event the dummy represents. It is also misleading to interpret the 
+# current Table 1 as it is.
+#
+# What I think you may need is an event study in time series or maybe 
+# a Difference-in-difference approach?
+# ------------------------------------------------------------------------------+
+
+# Tests for stationarity
+#
+gc.adf$p.value; gc.nt.adf$p.value; pa.adf$p.value; pa.net.adf$p.value
+# ... all 4 values above of p-value = 0.01
+summary(ur.df(gc, type = "drift", 6))
+summary(ur.df(gc.net, type = "drift", 6))
+summary(ur.df(pa, type = "drift", 6))
+summary(ur.df(pa.net, type = "drift", 6))
+# ... in all 4 lines above ==>> p-value: < 2.2e-16
+# hence, times series gc, gc.net, pa, pa.net are stationary
 
 library(stargazer)
-stargazer(m2,m3,m4,m5, type = "latex",
+stargazer(m4,m5,m2,m3, type = "text",
           title="Regression Results", single.row=FALSE,
-          ci=FALSE, ci.level=0.9, omit.stat=c("f", "ser"))
+          ci=FALSE, ci.level=0.9, omit.stat=c("f", "ser"))#, 
+#dep.var.labels=c("General Conditions","Putin Approval"))#,  
+#covariate.labels=c("New war dummy","New land gain"))
